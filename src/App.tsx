d534c1aa5028872df13fb1c3e161521e8dbdf672
yashwardhanbhati-ai <270562920+yashwardhanbhati-ai@users.noreply.github.com>
2026-03-25 18:05:37 +0530

    Revert all changes from the past 7 days

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
+import { Courses } from './pages/Courses';
+import { FreeTest } from './pages/FreeTest';
+import { JoinBatch } from './pages/JoinBatch';
+import { SelectedNursingOfficers } from './pages/SelectedNursingOfficers';
+import { Events } from './pages/Events';
+import { OffersAlters } from './pages/OffersAlters';
+import { Podcast } from './pages/Podcast';
+import { FreeDemoContent } from './pages/FreeDemoContent';
+import { VacancyResults } from './pages/VacancyResults';
+import { Blog } from './pages/Blog';
+import { BlogPost } from './pages/BlogPost';
+import { SubjectTests } from './pages/SubjectTests';
+import { MockTests } from './pages/MockTests';
 import { RouterWrapper } from './next/RouterWrapper';
-import { InfoPage } from './pages/InfoPage';
 
 type AppProps = {
   initialPath?: string;
@@ -15,20 +27,19 @@ function App({ initialPath = '/' }: AppProps) {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="jinc-jodhpur-institute" element={<InfoPage />} />
-          <Route path="jinc-jodhpur-courses" element={<InfoPage />} />
-          <Route path="jinc-jodhpur-admission" element={<InfoPage />} />
-          <Route path="jinc-jodhpur-fees" element={<InfoPage />} />
-          <Route path="jinc-jodhpur-faculty" element={<InfoPage />} />
-          <Route path="nursing-coaching-jodhpur" element={<InfoPage />} />
-          <Route path="norcet-coaching-jodhpur" element={<InfoPage />} />
-          <Route path="rrb-nursing-coaching-jodhpur" element={<InfoPage />} />
-          <Route path="nursing-officer-coaching-jodhpur" element={<InfoPage />} />
-          <Route path="aiims-norcet-exam-overview" element={<InfoPage />} />
-          <Route path="rrb-nursing-exam-overview" element={<InfoPage />} />
-          <Route path="nursing-officer-exam-overview" element={<InfoPage />} />
-          <Route path="jinc-jodhpur-admission-process" element={<InfoPage />} />
-          <Route path="student-experience" element={<InfoPage />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="freetest" element={<FreeTest />} />
+          <Route path="subject-tests" element={<SubjectTests />} />
+          <Route path="mock-tests" element={<MockTests />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="blog/:slug" element={<BlogPost />} />
+          <Route path="join-batch" element={<JoinBatch />} />
+          <Route path="selected-nursing-officers" element={<SelectedNursingOfficers />} />
+          <Route path="events" element={<Events />} />
+          <Route path="offers-alters" element={<OffersAlters />} />
+          <Route path="podcast" element={<Podcast />} />
+          <Route path="free-demo-content" element={<FreeDemoContent />} />
+          <Route path="vacancy-results" element={<VacancyResults />} />
         </Route>
       </Routes>
     </RouterWrapper>
